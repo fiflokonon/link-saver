354d08bedab9ff2f5f476a2bb3181606c2e9ebcf
Arnaud LOKONON <fiflokonon@gmail.com>
2025-11-23 23:14:42 +0100

    Generic links by routes

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::get("/countries", [CountryController::class, 'index']);
 Route::get("/countries/{id}/links", [GenericLinkController::class, 'links_by_country']);
+Route::get("/generic/links", [GenericLinkController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
